e497364ddfeafa717d57b772740b0c3f95bb27d3
yuanqi <yqi19980755776@163.com>
2020-01-19 17:22:43 +0800

    basecloud项目创建

---
+++ BaseCloud/src/main/java/com/yq/bc/BaseCloudApplication.java
@@ -0,0 +1,13 @@
+package com.yq.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseCloudApplication.class, args);
+    }
+
+}
---
+++ BaseCloud/src/test/java/com/yq/bc/BaseCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yq.bc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
